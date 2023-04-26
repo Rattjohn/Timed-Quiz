dd854cf2b2496f0ec7d2231f92bc049caf4964f5
ian rattigan <ian.rattigan2000@gmail.com>
2023-04-25 22:53:34 -0400

    add my base/structure of the game along with linked files

---
+++ Assets/script.js
@@ -0,0 +1,41 @@
+const questions = [
+    {
+        question: "What are RGB's main colors?",
+        answers: [
+            {text: "Red, Blue, Green", correct: true},
+            {text: "Yellow, Purple, Baby Blue", correct: false},
+            {text: "White, Blue , Purple", correct: false},
+            {text: "Green, White, Yellow", correct: false},
+        ]
+    },
+    {
+        question: "What are RGB's main colors?",
+        answers: [
+            {text: "Red, Blue, Green", correct: true},
+            {text: "Yellow, Purple, Baby Blue", correct: false},
+            {text: "White, Blue , Purple", correct: false},
+            {text: "Green, White, Yellow", correct: false},
+        ]
+    },
+    {
+        question: "What are RGB's main colors?",
+        answers: [
+            {text: "Red, Blue, Green", correct: true},
+            {text: "Yellow, Purple, Baby Blue", correct: false},
+            {text: "White, Blue , Purple", correct: false},
+            {text: "Green, White, Yellow", correct: false},
+        ]
+    },
+    {
+        question: "What are RGB's main colors?",
+        answers: [
+            {text: "Red, Blue, Green", correct: true},
+            {text: "Yellow, Purple, Baby Blue", correct: false},
+            {text: "White, Blue , Purple", correct: false},
+            {text: "Green, White, Yellow", correct: false},
+        ]
+    }
+]
+
+
+
