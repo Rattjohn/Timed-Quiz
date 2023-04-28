86eb0e49ab7f1bafedb4cf890fe2e9c2988bdc8b
ian rattigan <ian.rattigan2000@gmail.com>
2023-04-27 22:47:05 -0400

    added logic to show current questions and answers when starting quiz

--- Assets/script.js
+++ Assets/script.js
@@ -9,33 +9,76 @@ const questions = [
         ]
     },
     {
-        question: "What are RGB's main colors?",
+        question: "Which animal is known for its long neck?",
         answers: [
-            {text: "Red, Blue, Green", correct: true},
-            {text: "Yellow, Purple, Baby Blue", correct: false},
-            {text: "White, Blue , Purple", correct: false},
-            {text: "Green, White, Yellow", correct: false},
+            {text: "Ant Eater", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: true},
+            {text: "Sloths", correct: false},
         ]
     },
     {
-        question: "What are RGB's main colors?",
+        question: "Which is the Best Coding Bootcamp?",
         answers: [
-            {text: "Red, Blue, Green", correct: true},
-            {text: "Yellow, Purple, Baby Blue", correct: false},
-            {text: "White, Blue , Purple", correct: false},
-            {text: "Green, White, Yellow", correct: false},
+            {text: "Coding Dojo", correct: false},
+            {text: "BrainStation", correct: false},
+            {text: "University of Miami", correct: true},
+            {text: "Ironhack", correct: false},
         ]
     },
     {
-        question: "What are RGB's main colors?",
+        question: "What does the HTML consist of?",
         answers: [
-            {text: "Red, Blue, Green", correct: true},
-            {text: "Yellow, Purple, Baby Blue", correct: false},
-            {text: "White, Blue , Purple", correct: false},
-            {text: "Green, White, Yellow", correct: false},
+            {text: "Classes/ID's", correct: false},
+            {text: "Open/Closed tags", correct: false},
+            {text: "elements", correct: false},
+            {text: "All of The Above", correct: true},
         ]
     }
-]
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answerbuttons");
+const nextButton = document.getElementById("nextbtn");
+const startButton = document.getElementById("startbutton");
+
+let currentQuestionIndex = 1
+let score = 75;
 
 
 
+// need to figure out how to run showQuestion function everytime currentQuestionIndex increases
+function showQuestion(){
+    questionElement.innerHTML = questions[currentQuestionIndex].question
+
+    for (i=0; i < questions[currentQuestionIndex].answers.length; i++ ){
+        var button = document.createElement("button");
+        button.innerHTML = questions[currentQuestionIndex].answers[i].text
+        answerButton.appendChild(button);
+        button.addEventListener("click", function() {
+            currentQuestionIndex++
+            showQuestion();
+        })
+    }
+// add logic to increase score when user gets correct answer and remove time when incorrect
+    
+
+    
+}
+
+function isCorrect() {
+    
+}
+
+function startQuiz(){
+    startButton.style.display = "none"
+    for(var i = 0; i<answerButton.children.length; i++){
+        answerButton.children[i].setAttribute("class","btn")
+    }
+    showQuestion();
+
+};
+
+
+
+startButton.addEventListener("click",startQuiz)
